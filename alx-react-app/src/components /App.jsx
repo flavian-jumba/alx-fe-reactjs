eeca356141d45f61ff0ef86855b0bff4db69be6e
Flavian Jumba <flaviajumba@gmail.com>
2024-08-16 20:04:13 +0300

    made the React App

--- alx-react-app/src/components /App.jsx
+++ alx-react-app/src/components /App.jsx
@@ -2,6 +2,7 @@ import WelcomeMessage from './WelcomeMessage'
 import Header from './Header';
 import MainContent from './MainContent';
 import Footer from './Footer';
+import UserProfile from './UserProfile';  
 function App() {
   return (
     <>
@@ -9,6 +10,7 @@ function App() {
     <Header/>
     <MainContent/>
     <Footer/>
+    <UserProfile name="Alice" age="25" bio="Loves hiking and photography"/>
     </>
   )
 }
---
+++ alx-react-app/src/components /UserProfile.jsx
@@ -0,0 +1,13 @@
+
+
+function UserProfile(props) {
+  return (
+    <>
+    <h2>name: {props.name}</h2>
+    <p>Age: {props.age}</p>
+    <p>Bio: {props.bio}</p>
+    </>
+  )
+}
+
+export default UserProfile
