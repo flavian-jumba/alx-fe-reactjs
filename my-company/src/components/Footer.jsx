b92aead3a818d84c06204ee9a39f8f48ca6f7589
Flavian Jumba <flaviajumba@gmail.com>
2024-08-24 01:27:25 +0300

    made app

---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+    return (
+      <footer style={{ padding: '10px', backgroundColor: '#ddd', textAlign: 'center' }}>
+        <p>&copy; 2024 Our Company. All rights reserved.</p>
+      </footer>
+    );
+  }
+  
+  export default Footer;
+  
