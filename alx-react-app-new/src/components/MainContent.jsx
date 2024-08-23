52113c406480df479b7bf928d05f47c410be0242
Flavian Jumba <flaviajumba@gmail.com>
2024-08-24 00:20:52 +0300

    made the app

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,7 +1,7 @@
 
 function MainContent() {
   return (
-    <div style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center' }}>I love to visit New York, Paris, and Tokyo.</div>
+    <div style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center' }}>I love to visit New York, Paris, and Tokyo.main</div>
   )
 }
 
