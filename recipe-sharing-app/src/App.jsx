2a3fd80c65c6bfa3b25b7fbc89f6ffff933684a6
Flavian Jumba <flaviajumba@gmail.com>
2024-08-29 11:37:29 +0300

    made the app

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,14 +1,21 @@
 // src/App.jsx
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
+import RecipeDetails from './components/RecipeDetails'; // Importing the RecipeDetails component
 
 const App = () => {
   return (
-    <div className="app-container">
-      <h1>Recipe Manager</h1>
-      <AddRecipeForm />
-      <RecipeList />
-    </div>
+    <Router>
+      <div className="app-container">
+        <h1>Recipe Manager</h1>
+        <AddRecipeForm />
+        <Routes>
+          <Route path="/" element={<RecipeList />} /> {/* Route for the main recipe list */}
+          <Route path="/recipe/:recipeId" element={<RecipeDetails />} /> {/* Route for viewing individual recipe details */}
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
