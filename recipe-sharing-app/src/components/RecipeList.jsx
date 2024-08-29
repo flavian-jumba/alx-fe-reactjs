07248c8d0489c7e9dccde5c7ca526468109dcc38
Flavian Jumba <flaviajumba@gmail.com>
2024-08-29 11:46:26 +0300

    Enhancing the Recipe Sharing Application with Detailed Recipe Management Implementing Advanced Search and Filtering Capabilities

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,5 +1,6 @@
 // src/components/RecipeList.jsx
 import { useRecipeStore } from './recipeStore';
+import { Link } from 'react-router-dom'; // Importing Link from react-router-dom
 
 const RecipeList = () => {
   const filteredRecipes = useRecipeStore((state) => state.filteredRecipes);
@@ -8,7 +9,9 @@ const RecipeList = () => {
     <div>
       {filteredRecipes.map((recipe) => (
         <div key={recipe.id}>
-          <h3>{recipe.title}</h3>
+          <h3>
+            <Link to={`/recipe/${recipe.id}`}>{recipe.title}</Link> {/* Adding Link to navigate to recipe details */}
+          </h3>
           <p>{recipe.description}</p>
         </div>
       ))}
