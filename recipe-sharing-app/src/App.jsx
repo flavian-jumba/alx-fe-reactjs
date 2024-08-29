a04224335e116b1eafae3f4d7ffaa732510e7ecc
Flavian Jumba <flaviajumba@gmail.com>
2024-08-29 11:20:28 +0300

    made the app

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,6 +1,6 @@
 // src/App.jsx
-import RecipeList from './RecipeList';
-import AddRecipeForm from './AddRecipeForm';
+import RecipeList from './components/RecipeList';
+import AddRecipeForm from './components/AddRecipeForm';
 
 const App = () => {
   return (
