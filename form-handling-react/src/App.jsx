4bab61d1e2e72841ad35a1c8153cf97ddb93a3ad
Mr Jumba <flaviajumba@gmail.com>
2024-09-08 23:18:06 +0300

    made the FormikForm

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import RegistrationForm from './components/RegistrationForm'
+import FormikForm from './components/FormikForm'
 function App() {
   return (
    <>
    <RegistrationForm />
+    <FormikForm/>
    </>
   )
 }
